0a5341d1209b288565b340f2c7d85af5a311885f
Jason Kayser <jpk2f2@mail.missouri.edu>
2019-10-29 08:28:22 -0500

    Added comments; Final presentation preparation

--- main.py
+++ main.py
@@ -3,8 +3,11 @@ import matplotlib
 import utility
 import threshold
 
+from matplotlib import pyplot as plt
+
 matplotlib.use('TkAgg')
 
+# read in example images as grayscale
 im_1001a = cv2.imread('resources/Fig1001(a)(constant_gray_region).tif', 0)
 im_1001d = cv2.imread('resources/Fig1001(d)(noisy_region).tif', 0)
 im_1022a = cv2.imread('resources/Fig1022(a)(building_original).tif', 0)
@@ -16,22 +19,30 @@ im_1043a = cv2.imread('resources/Fig1043(a)(yeast_USC).tif', 0)
 im_1045a = cv2.imread('resources/Fig1045(a)(iceberg).tif', 0)
 im_1060a = cv2.imread('resources/Fig1060(a)(car on left).tif', 0)
 
+# group example images and prepare names
 images = [im_1001a, im_1001d, im_1022a, im_1026a, im_1027a, im_1030a, im_1034a, im_1043a, im_1045a, im_1060a]
 titles = ['im_1001a', 'im_1001d', 'im_1022a', 'im_1026a', 'im_1027a', 'im_1030a', 'im_1034a', 'im_1043a',
           'im_1045a', 'im_1060a']
 
 
+# for presentation
+# apply global thresholding on list of images
+# takes in list of images, and difference for global thresholding
+# returns list of original and processed images
 def apply_global_thresholding(ims: list, diff: int):
-    ims_processed = []
+    ims_processed = []  # hold original and completed images
     for im in ims:
-        print('Processing image w/ global thresholding')
-        ims_processed.append(im)
-        ims_processed.append(threshold.global_threshold_apply(im, 2))
+        print('Processing image w/ global thresholding')  # track image progress
+        ims_processed.append(im)  # append original image
+        ims_processed.append(threshold.global_threshold_apply(im, 2))  # process and append new image
 
     print('Completed all images\n')
     return ims_processed
 
 
+# for presentation
+# display list of images, one set at a time
+# takes in list of images for display, list of display names, size of each set, and type of alg used on input images
 def display_imageset(ims: list, names: list, step: int, alg: str):
     i = 0
     j = 0
@@ -42,6 +53,7 @@ def display_imageset(ims: list, names: list, step: int, alg: str):
             if i % step == 0:
                 cv2.imshow(names[j] + ' ' + names_global[i % step], im)
             else:
+                # normalize global thresholded image
                 cv2.imshow(names[j] + ' ' + names_global[i % step],
                            cv2.normalize(im, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8UC1))
         else:
@@ -55,11 +67,16 @@ def display_imageset(ims: list, names: list, step: int, alg: str):
     return
 
 
+# for presentation
+# apply K-Mean Clustering algorithm on given list of images
+# takes in list of input images, type of initial cluster allocation, and number of cluster
+# k = 0 runs the algorithm twice with both a k of 3 and a k of 5
+# returns a list of original image - processed image sets
 def apply_k_means(ims: list, initial: str, k: int = 0):
-    ims_processed = []
+    ims_processed = []  # holds original and processed images
     if k == 0:
         for im in ims:
-            print('Processing image w/ k-means')
+            print('Processing image w/ k-means')  # track progress
             ims_processed.append(im)
             ims_processed.append(threshold.k_means(im, 3, initial))
             ims_processed.append(threshold.k_means(im, 5, initial))
@@ -72,6 +89,8 @@ def apply_k_means(ims: list, initial: str, k: int = 0):
     return ims_processed
 
 
+# writes images given in list to 'plots' folder under ' resources'
+# takes in list of images to be written, names of images, size of image sets, and type of alg run on input images
 def write_imageset(ims: list, names: list, step: int, alg: str):
     i = 0
     j = 0
@@ -82,6 +101,7 @@ def write_imageset(ims: list, names: list, step: int, alg: str):
             if i % step == 0:
                 cv2.imwrite('resources/plots/{}_{}.png'.format(names[j], names_global[i % step]), im)
             else:
+                # normalize global thresholded image
                 cv2.imwrite('resources/plots/{}_{}.png'.format(names[j], names_global[i % step]),
                             cv2.normalize(im, None, 255, 0, cv2.NORM_MINMAX, cv2.CV_8UC1))
         else:
@@ -93,12 +113,18 @@ def write_imageset(ims: list, names: list, step: int, alg: str):
     return
 
 
-# run global threshold algorithm on image set
-# display processed images next to their original counterparts
-# display_imageset(apply_global_thresholding(images, 2), titles, 2, 'global')
-ims_global = apply_global_thresholding(images, 2)
-display_imageset(ims_global, titles, 2, 'global')
-write_imageset(ims_global, titles, 2, 'global')
+# the commented block below is for generating histograms on the k means clustering algorithm
+'''
+plt.hist(im_1060a.ravel(), 256, [0, 256])
+plt.show()
+im_test = threshold.k_means(im_1060a, 3, 'spaced')
+plt.hist(im_test.ravel(), 256, [0, 256])
+plt.show()
+im_test2 = threshold.k_means(im_1060a, 5, 'spaced')
+plt.hist(im_test2.ravel(), 256, [0, 256])
+plt.show()
+'''
+
 
 # run k cluster algorithm on image set
 # display processed images next to their original counterparts
@@ -107,6 +133,15 @@ ims_k = apply_k_means(images, 'spaced')
 display_imageset(ims_k, titles, 3, 'k-means')
 write_imageset(ims_k, titles, 3, 'k-means')
 
+# run global threshold algorithm on image set
+# display processed images next to their original counterparts
+# display_imageset(apply_global_thresholding(images, 2), titles, 2, 'global')
+ims_global = apply_global_thresholding(images, 2)
+display_imageset(ims_global, titles, 2, 'global')
+write_imageset(ims_global, titles, 2, 'global')
+
+
+# old code below, kept in case needed for presenting
 
 # im_test = threshold.global_threshold_apply(im_1001a, 2)
 # im_test = threshold.k_means_intial(im_1060a, 3, 'spaced')
--- threshold.py
+++ threshold.py
@@ -2,20 +2,30 @@ import numpy as np
 import random
 
 
+# finds average gray value of entire imagae
+# takes in image array
+# returns average grey value
 def find_grey_avg(im: np.ndarray):
-    dimensions = im.shape
-    avg = np.sum(im) / (dimensions[0] * dimensions[1])
+    dimensions = im.shape  # get size of image
+    avg = np.sum(im) / (dimensions[0] * dimensions[1])  # sum image values and divide by total
 
     return int(avg)
 
 
+# find global threshold value T of a given image
+# takes in an image array, minimum difference in successive thresholds, and an intial threshold
+# recommend using find_grey_avg() for the initial threshold t
+# returns the global threshold
 def find_global_threshold(im: np.ndarray, diff: int, t: int):
+    # initialize counters/value holders
     t_prev = t
     count1 = 0
     count2 = 0
     sum1 = 0
     sum2 = 0
 
+    # iterate image array
+    # split on current threshold and average seperately
     with np.nditer(im, op_flags=['readonly']) as it:
         for x in it:
             if x[...] > t:
@@ -25,15 +35,20 @@ def find_global_threshold(im: np.ndarray, diff: int, t: int):
                 count2 += 1
                 sum2 += x[...]
 
-    t = int(((sum1 / count1 + sum2 / count2) / 2))
+    t = int(((sum1 / count1 + sum2 / count2) / 2))  # recombine and normalize
+    # check if less than predefined difference
     if abs(t - t_prev) < diff:
-        return t
+        return t  # break recursion
     else:
-        return find_global_threshold(im, diff, t)
+        return find_global_threshold(im, diff, t)  # recursion
 
 
+# sets image values based on threshold value
+# takes in image array and threshold T
+# returns processed image, all values 0 or 1
 def global_threshold(im: np.ndarray, t: int):
-    im2 = im.copy()
+    im2 = im.copy()  # prevent altering original image
+    # iterate image array and change values
     with np.nditer(im2, op_flags=['readwrite']) as it:
         for x in it:
             x[...] = 1 if x[...] > t else 0
@@ -41,62 +56,65 @@ def global_threshold(im: np.ndarray, t: int):
     return im2
 
 
+# applies all functions necessary to conduct complete Basic Global Thresholding algorithm
+# takes in image array and difference for use in finding the global threshold value
 def global_threshold_apply(im: np.ndarray, diff: int):
-    t = find_global_threshold(im, diff, find_grey_avg(im))
+    t = find_global_threshold(im, diff, find_grey_avg(im))  # find global threshold using avg grey value to start
 
+    # apply global threshold and return
     return global_threshold(im, t)
 
 
-# placing this here to remember
-# dist = numpy.linalg.norm(a-b)
-
-
+# K-Means Clustering algorithm
+# Segments image based on K number of cluster
+# takes in an image array, K number of clusters, and the type of cluster initialization
 def k_means(im: np.ndarray, k: int, initial: str):
+    # get max and min values of image for cluster initialization
     max_val = np.max(im)
     min_val = np.min(im)
-    dimensions = im.shape
+    dimensions = im.shape  # get size of image
     total = dimensions[0] * dimensions[1]
-    im_flat = im.flatten()
+    im_flat = im.flatten()  # flatten image for processing
+    # initalize clusters
     centroids = np.zeros([k])
     centroids_prev = np.zeros([k])
 
-    if initial == 'rand':
-        centroids = random.sample(range(0, 256), k)
-        # for i in range(k):
-        #    rand = random.randint(0, total-1)
-        #    centroids[i] = im_flat[rand]
-    elif initial == 'spaced':
+    if initial == 'rand':  # randomly select values within the image range
+        centroids = random.sample(range(min_val, max_val+1), k)
+    elif initial == 'spaced':  # select K evenly spaced values within the image range
         idx = np.round(np.linspace(min_val, max_val, k)).astype(int)
         centroids = idx
-        # idx = np.round(np.linspace(0, len(im_flat) - 1, k)).astype(int)
-        # for i in range(k):
-        #   print(idx[i])
-        # centroids = im_flat[idx]
 
+    # holds cluster association for each pixel of image
     cluster_array = np.zeros([total], dtype='int8')
 
+    # loop until cluster centers stop moving
     while not np.array_equiv(centroids, centroids_prev):
         centroids_prev = centroids
+
+        # calculate distance between each point and each cluster center
+        # keep minimum value for each
         for i in range(im_flat.size):
             dist_array = np.zeros([k])
             for j in range(k):
                 dist_array[j] = np.linalg.norm(im_flat[i]-centroids[j])
             cluster_array[i] = int(np.argmin(dist_array))
-            #print(cluster_array[i])
 
+        # set new cluster centers based on associated pixels
         for i in range(k):
             count = 0
             summ = 0
             for j in range(cluster_array.size):
-                if k == cluster_array[j]:
+                if i == cluster_array[j]:
                     count += 1
                     summ += im_flat[j]
                 centroids[i] = int(summ/count) if count > 0 else centroids[i]
-
+    # assign pixel values to associated cluster center values
     for i in range(k):
         for j in range(im_flat.size):
             if cluster_array[j] == i:
                 # print('here')
                 im_flat[j] = centroids[i]
 
+    # reshape and return processed image array
     return im_flat.reshape([dimensions[0], dimensions[1]])
